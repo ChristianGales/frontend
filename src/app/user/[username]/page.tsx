ee10f0ee363d30a5c90068303e5ae020758728d2
charles <etchangales@gmail.com>
2026-05-12 15:32:09 +0800

    user page 20%

--- src/app/user/[username]/page.tsx
+++ src/app/user/[username]/page.tsx
@@ -1,5 +1,9 @@
 import CardList from "@/components/CardList";
+import { Badge } from "@/components/ui/badge";
 import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "@/components/ui/breadcrumb";
+import { HoverCard, HoverCardContent, HoverCardTrigger } from "@/components/ui/hover-card";
+import { Progress } from "@/components/ui/progress";
+import { BadgeCheck } from "lucide-react";
 
 const SingleUserpage = () => {
     return (
@@ -30,11 +34,86 @@ const SingleUserpage = () => {
                 <div className="w-full xl:w-1/3 space-y-6">
                     {/* user badges */}
                     <div className="bg-primary-foreground p-4 rounded-lg">
-                        <h1 className="">User Badges</h1>
+                        <h1 className="text-xl font-semibold">User Badges</h1>
+                        
+                            {/* hover cards badges*/}
+                            <div className="flex gap-4 mt-4">
+                                <HoverCard>
+                                    <HoverCardTrigger>
+                                        <BadgeCheck size="36" className="rounded-full bg-blue-500/30 border-1 border-blue-500/50 p-2"/>
+                                    </HoverCardTrigger>
+                                    <HoverCardContent>
+                                       <h1 className="font-bold mb-2">Verified User</h1>
+                                       <p className="text-sm text-muted-foreground">This user has been verfied by me</p>
+                                    </HoverCardContent>
+                                </HoverCard>
+
+                                <HoverCard>
+                                    <HoverCardTrigger>
+                                        <BadgeCheck size="36" className="rounded-full bg-red-500/30 border-1 border-red-500/50 p-2"/>
+                                    </HoverCardTrigger>
+                                    <HoverCardContent>
+                                       <h1 className="font-bold mb-2">Verified User</h1>
+                                       <p className="text-sm text-muted-foreground">This user has been verfied by me</p>
+                                    </HoverCardContent>
+                                </HoverCard>
+
+                                <HoverCard>
+                                    <HoverCardTrigger>
+                                        <BadgeCheck size="36" className="rounded-full bg-green-500/30 border-1 border-blue-green/50 p-2"/>
+                                    </HoverCardTrigger>
+                                    <HoverCardContent>
+                                       <h1 className="font-bold mb-2">Verified User</h1>
+                                       <p className="text-sm text-muted-foreground">This user has been verfied by me</p>
+                                    </HoverCardContent>
+                                </HoverCard>
+
+                                <HoverCard>
+                                    <HoverCardTrigger>
+                                        <BadgeCheck size="36" className="rounded-full bg-orange-500/30 border-1 border-orange-500/50 p-2"/>
+                                    </HoverCardTrigger>
+                                    <HoverCardContent>
+                                       <h1 className="font-bold mb-2">Verified User</h1>
+                                       <p className="text-sm text-muted-foreground">This user has been verfied by me</p>
+                                    </HoverCardContent>
+                                </HoverCard>
+                            </div>
                     </div>
 
                     {/* info  */}
-                    <div className="bg-primary-foreground p-4 rounded-lg">Info</div>
+                    <div className="bg-primary-foreground p-4 rounded-lg">
+                         <h1 className="text-xl font-semibold">User Information</h1>
+                         <div className="space-y-4 mt-4">
+                            <div className="flex flex-col gap-2 mb-8">
+                                <p className="text-sm text-muted-foreground">Profile Completion</p>
+                                <Progress value={66}/>
+                            </div>
+                            <div className="flex item-center gap-2">
+                                <span className="font-bold">Username</span>
+                                <span>John.doe</span>
+                            </div>
+                             <div className="flex item-center gap-2">
+                                <span className="font-bold">Email</span>
+                                <span>John.doe</span>
+                            </div>
+
+                             <div className="flex item-center gap-2">
+                                <span className="font-bold">Phone</span>
+                                <span>John.doe</span>
+                            </div>
+
+                             <div className="flex item-center gap-2">
+                                <span className="font-bold">Location</span>
+                                <span>John.doe</span>
+                            </div>
+
+                             <div className="flex item-center gap-2">
+                                <span className="font-bold">Role</span>
+                                <Badge>Admin</Badge>
+                            </div>
+                         </div>
+                         <p className="text-sm text-muted-foreground">Joined on 202501.01</p>
+                    </div>
 
                      {/* card list  */}
                     <div className="bg-primary-foreground p-4 rounded-lg">
