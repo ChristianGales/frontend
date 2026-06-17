160f0cfcd901a51c4c0e44f1ecd4cc419ef6f5ee
Christian Gales <etchangales@gmail.com>
2026-06-17 23:19:45 +0800

    admin>user page added users fixed

--- src/lib/dummy/admin/user.ts
+++ src/lib/dummy/admin/user.ts
@@ -1,3 +1,5 @@
+import { User } from "@/types/admin/user"
+
 export const users: User[] = [
   { id: "NSC26-0001", fullname: "Juan Dela Cruz", email: "juan.delacruz@nsci.edu.ph", user_type: "FACULTY" },
   { id: "NSC26-0002", fullname: "Maria Clara Reyes", email: "maria.reyes@nsci.edu.ph", user_type: "COLLEGE REGISTRAR" },
